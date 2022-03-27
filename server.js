4dd6b233f46b9efd301dbe5eefd2fc7878a970e8
pokerhawk <eliabedosreis@gmail.com>
2022-03-26 22:40:42 -0300

    updating DATABASE

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = require('knex');
 const knex = db({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-elliptical-86900',
         port : 5432,
         user : 'postgres',
         password : 'xiaoxiao10',
